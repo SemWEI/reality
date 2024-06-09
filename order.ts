2a9ff9684814e75c971e8350e45ac795460c5071
Anupama Gamage <anupamag@alliontechnologies.com>
2024-06-09 16:23:00 +0000

    e com schemas

---
+++ order.ts
@@ -0,0 +1,22 @@
+type Order = {
+    //acceptedOffer?: Offer;
+    //billingAddress?: PostalAddress;
+    //broker?: Organization | Person;
+    confirmationNumber?: string;
+    //customer?: Organization | Person;
+    discount?: number | string;
+    discountCode?: string;
+    discountCurrency?: string;
+    isGift?: boolean;
+    //orderDate?: Date | DateTime;
+    //orderDelivery?: ParcelDelivery;
+    orderNumber?: string;
+    //orderStatus?: OrderStatus;
+    //orderedItem?: OrderItem | Product | Service;
+    //partOfInvoice?: Invoice;
+    //paymentDueDate?: Date | DateTime;
+    //paymentMethod?: PaymentMethod;
+    paymentMethodId?: string;
+    paymentUrl?: URL;
+    //seller?: Organization | Person;
+};  
