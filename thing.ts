9ef0c3d2a96ced1f8ccd239212a382b520fcbdc0
Anupama Gamage <anupamag@alliontechnologies.com>
2024-09-05 01:36:21 +0530

    Added thing

---
+++ thing.ts
@@ -0,0 +1,14 @@
+type Thing = {
+    additionalType?: string | URL;
+    alternateName?: string;
+    description?: string | TextObject;
+    disambiguatingDescription?: string;
+    identifier?: PropertyValue | string | URL;
+    image?: ImageObject | URL;
+    mainEntityOfPage?: CreativeWork | URL;
+    name?: string;
+    potentialAction?: Action;
+    sameAs?: URL;
+    subjectOf?: CreativeWork | Event;
+    url?: URL;
+}
